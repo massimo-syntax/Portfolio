470f9475a9b27ab04228d732c6b383f179f22930
massimo-syntax <josartorio@proton.me>
2025-03-31 23:04:15 +0200

    detect section scrolling, highlight proper nav button

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ document.addEventListener("DOMContentLoaded", (event) => {
 	//    FRAMEWORK
 	const d = (element_query_selector) => document.querySelector(element_query_selector)
 	const dl = (element_query_selector) => document.querySelectorAll(element_query_selector)
-
 	const log = (what) => {console.log(what)}
 	 
 	//    nabar width for scrolling on nav click
@@ -26,12 +25,11 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
 	gsap.to(".char", {
 		y: 0,
-		opacity:1,
 		stagger: 0.05,
 		delay: 0.2,
 		duration: 0.1,
 	});
-
+	
 	gsap.to('.box-reveal', {
 	  duration: 1.5,
 	  opacity:1,
@@ -44,27 +42,67 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
 
 	let timeLast = Date.now()
+	let time = timeLast
 
-	document.addEventListener("scroll", (event) => {
-		
-		log(window.scrollY + navbarHeight)
 
+	const sections = dl(".vanillaJsScroll")
+	const heights = []
+	const tops = []
+	sections.forEach(s => { 
+		heights.push(s.offsetTop+s.offsetHeight - s.offsetTop)  
+		tops.push(s.offsetTop)
+	})
+
+	heights.forEach( (h, index) => {log(h); log(tops[index]) } )
+
+	const count = sections.length
+
+	let scroll = 0
+
+
+	const btns = dl("button.scroll-to")
+	let lastBtn = btns[btns.length-1]
+	const _i_color = lastBtn.style.color
+	const prettyColor = "#146ef5"
+
+
+
+
+	const documentBottom = Math.max( document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight );
+
+	window.addEventListener("scroll", (event) => {
+		// the browser can rest some milliseconds
+		time = Date.now()
+		if( time > timeLast + 200 ){
+			// top window + navbar does not directly overlap tops[i]
+			scroll = window.scrollY + navbarHeight + 30; 
+			for(i = 0 ; i < sections.length ; i++ ){
+
+				if(scroll > tops[i] && scroll < tops[i]+heights[i]){
+					//	lastBtn = btns[i]
+					if(btns[i] !== lastBtn){
+						//btns[i].focus()
+						btns[i].style.color = prettyColor
+						lastBtn.style.color = _i_color
+						lastBtn.blur()
+						lastBtn = btns[i]
+
+					}
+
+				} 
+			}
+			timeLast = time
+		}
+
+		if(scroll < navbarHeight + 50) scrollbar.style.left = btns[0].offsetLeft + "px"
+		if( (scroll - navbarHeight + window.innerHeight) > documentBottom ) scrollbar.style.left = btns[btns.length-1].offsetLeft + btns[btns.length-1].offsetWidth - (halfScrollbar*2) + "px"
+
+		//log( documentBottom + " --- " + ( scroll - navbarHeight + window.innerHeight ) )
 
   	});
 
-	const scrollToBtns = dl("nav button.scroll-to")
-	const sections = dl(".content h2")
-
-	log(scrollToBtns)
-	log(sections)
 
 
-	// all buttons 
-	// scroll position > of top ,  < of bottom
-  
-  
-  
-  
   
 });
 
